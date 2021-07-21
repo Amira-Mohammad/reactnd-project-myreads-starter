8bb7ca894f1155c2f355c30e28d152815a381e48
Amira Mohamed <a.mohamed@penta-b.com>
2021-07-21 19:38:50 +0200

    Updates

---
+++ precache-manifest.b7e839bb950065abd2d44b167c6d4d49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fa6dcd2f89806da0727",
+    "url": "/reactnd-project-myreads-starter/static/css/main.b1c794da.chunk.css"
+  },
+  {
+    "revision": "3fa6dcd2f89806da0727",
+    "url": "/reactnd-project-myreads-starter/static/js/main.3fa6dcd2.chunk.js"
+  },
+  {
+    "revision": "a20acc9f2babc1227fd2",
+    "url": "/reactnd-project-myreads-starter/static/js/1.a20acc9f.chunk.js"
+  },
+  {
+    "revision": "83f7d75ca12013b90cc6",
+    "url": "/reactnd-project-myreads-starter/static/js/runtime~main.83f7d75c.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/reactnd-project-myreads-starter/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "8df17b712222d57cbdde173781fe8b77",
+    "url": "/reactnd-project-myreads-starter/index.html"
+  }
+];
